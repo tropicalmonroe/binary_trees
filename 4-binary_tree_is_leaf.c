d0816f954aa9198aee0d81d216ccce7442899401
tropicalmonroe <shitsamajohn821@gmail.com>
2022-09-21 06:42:10 -0700

    Binary tree complete

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - a function that checks if a node is a leaf
+ * @node: is a pointer to the node to check
+ * Return: 0 if node is NULL
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
+	return (1);
+}
