35a99e6ba76f261f6afa621c8069aa33e06546fb
tropicalmonroe <shitsamajohn821@gmail.com>
2022-09-21 23:02:12 -0700

    binary tree complete

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: a pointer to the root node of the tree to count the number of nodes
+ * Return: If tree is NULL, the function must return 0
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_leaves(tree->right)
+		+ binary_tree_leaves(tree->left));
+}
