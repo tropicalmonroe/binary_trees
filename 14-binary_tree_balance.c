b0e9413144b88036d45bd70f38012c473842b85e
tropicalmonroe <shitsamajohn821@gmail.com>
2022-09-22 04:21:22 -0700

    Binary tree complete

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,17 +9,17 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
-		return (binary_tree_balance(tree->left) - binary_tree_balance(tree-right));
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
 	return (0);
 }
 
 /**
- * binary_tree_height - func tht measures the height of a binary tree
- * @tree: root node of the tree to measure the height
- * Return: If tree is NULL, return 0
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0, else return height.
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
