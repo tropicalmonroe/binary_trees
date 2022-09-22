bc5b720bf529877e806b3a5360d281c5f1fa7297
tropicalmonroe <shitsamajohn821@gmail.com>
2022-09-21 23:38:26 -0700

    binary tree complete fix

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,9 +12,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		node += (tree->left || tree->right) ? 1 : 0;
-		node += binary_tree_nodes(tree->left);
-		node += binary_tree_nodes(tree->right);
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
 	}
 	return (nodes);
 }
